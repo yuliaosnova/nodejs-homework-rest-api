3296a549a4e6054cb44d655770fdf0699b14fb72
Yulia Osnova <yulia.osnova@gmail.com>
2023-05-27 12:50:48 +0300

    FIXED: deleted extra erros in catch block

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -26,9 +26,6 @@ router.get("/", async (req, res, next) => {
     const result = await contacts.listContacts();
     res.json(result);
   } catch (error) {
-    res.status(500).json({
-      message: "Server error",
-    });
     next(error);
   }
 });
@@ -44,10 +41,6 @@ router.get("/:id", async (req, res, next) => {
 
     res.json(result);
   } catch (error) {
-    const { status = 500, message = "Server error" } = error;
-    res.status(status).json({
-      message,
-    });
     next(error);
   }
 });
