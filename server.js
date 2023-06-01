2d121462c2076140ab883715b8cd10c31dfa4faf
Yulia Osnova <yulia.osnova@gmail.com>
2023-06-01 16:38:55 +0300

    ADDED: Add connection to db

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+// const DB_HOST =
+//   "mongodb+srv://Yulia:aEh68cJCvowYn4Dd@hw-rest-api.ckyqju9.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+const {DB_HOST, PORT = 3000} = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful!  ;)");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
