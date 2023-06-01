891501161531ef5c98549a8e8a70649d2caefc55
Yulia Osnova <yulia.osnova@gmail.com>
2023-06-01 15:15:42 +0300

    ADDED: add middleware

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,15 +2,19 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
+const { validateBody } = require("../../middlewares");
+
+const schemas = require("../../schemas");
+
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
 router.get("/:id", ctrl.getById);
 
-router.post("/", ctrl.add);
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:id", ctrl.updateById);
+router.put("/:id", validateBody(schemas.addSchema), ctrl.updateById);
 
 router.delete("/:id", ctrl.deleteById);
 
