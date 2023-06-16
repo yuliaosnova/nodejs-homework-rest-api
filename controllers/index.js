0b1cc21ee7e36c7ba132b3f6ce5f21745ca21ee7
Yulia Osnova <yulia.osnova@gmail.com>
2023-06-16 12:48:51 +0300

    FIXED: fixed according mentors comments

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const getAll = require("./getAll");
+const getById = require("./getById");
+const add = require("./add");
+const updateById = require("./updateById");
+const updateStatusContact = require("./updateStatus");
+const deleteById = require("./deleteById");
+
+module.exports = {
+  getAll,
+  getById,
+  add,
+  updateById,
+  updateStatusContact,
+  deleteById,
+};
